1d80d39a84a4f6910aae213c1bb7b558c1954e0c
Weshy984 <waweruguthua1@gmail.com>
2023-11-21 07:35:17 -0500

    Add firebase dependency and reusable widgets

--- lib/auth/signup.dart
+++ lib/auth/signup.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tiqiti/reusable_widgets/reusable_widgets.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -7,53 +8,79 @@ class SignUpScreen extends StatefulWidget {
   State<SignUpScreen> createState() => _SignUpScreenState();
 }
 
-class _SignUpScreenState extends State<SignUpScreen> {
+class _SignUpScreenState extends State<SignUpScreen>{
+final TextEditingController _nameTextController = TextEditingController();
+final TextEditingController _telephoneTextController = TextEditingController();
+final TextEditingController _emailTextController = TextEditingController();
+final TextEditingController _passwordTextController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-        backgroundColor: const Color(0xFFF1FAEE),
-        body: Column(
-          children: [
-            const SizedBox(
-              height: 70,
-            ),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Registration", style: TextStyle(
-                    fontSize: 48, fontWeight: FontWeight.w400),)
-              ],
-            ),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Join us Today!", style: TextStyle(
-                    fontSize: 30, fontWeight: FontWeight.w400),)
-              ],
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                    height: 43,
-                    padding: const EdgeInsets.only(left: 5),
-                    decoration: const BoxDecoration(color: Color(0xFFD9D9D9)),
-                    child: TextFormField(
-                      keyboardType: TextInputType.text,
-                      //controller: controller,
-                      style: const TextStyle(fontSize: 20,fontWeight: FontWeight.w400),
-                      decoration: const InputDecoration(
-                        hintText: "First Name",
-                      ),
-                    )
-                )
-              ],
-            )
 
-          ],
+        body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          decoration: const BoxDecoration(
+            color: Color(0x00f1faee),
+          ),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 70,
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Registration", style: TextStyle(
+                        fontSize: 48, fontWeight: FontWeight.w400),)
+                  ],
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Join us Today!", style: TextStyle(
+                        fontSize: 30, fontWeight: FontWeight.w400),)
+                  ],
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15,right: 15),
+                  child: reusableTextField("First Name", Icons.person, false, _nameTextController),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15,right: 15),
+                  child: reusableTextField("Last Name", Icons.person, false, _nameTextController),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15,right: 15),
+                  child: reusableTextField("Phone Number", Icons.phone, false, _telephoneTextController),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15,right: 15),
+                  child: reusableTextField("E-mail", Icons.mail, false, _emailTextController),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15,right: 15),
+                  child: reusableTextField("Password", Icons.lock, true, _passwordTextController),
+                ),
+              ],
+             ),
+        ),
         )
 
     );
