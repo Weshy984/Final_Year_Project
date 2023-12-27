f434efae1fb02ddff4a25087debbc2a3a99cf653
Weshy984 <waweruguthua1@gmail.com>
2023-12-27 14:30:31 +0300

    Implemeted UI for search screen

---
+++ lib/screens/seat_booking_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SeatBooking extends StatefulWidget {
+  const SeatBooking({super.key});
+
+  @override
+  State<SeatBooking> createState() => _SeatBookingState();
+}
+
+class _SeatBookingState extends State<SeatBooking> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
